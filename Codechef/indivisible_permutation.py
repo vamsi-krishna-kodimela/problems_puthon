6e8d5b55bdd764e6e0c0f3e8fabce5c6132cb0ae
vamsi-krishna-kodimela <vamsikrishnakdml@gmail.com>
2022-02-25 10:43:46 +0530

    Indivisible permutation updated

--- Codechef/indivisible_permutation.py
+++ Codechef/indivisible_permutation.py
@@ -1,7 +1,7 @@
 T = int(input())
-for i in range(T):
+for _ in range(T):
     n = int(input())
-    for i in range(1, n, 2):
-        print(i+1, end=" ")
-        print(i+2, end=" ")
-    print(1, end="\n")
+    for i in range(1,n):
+        print(i+1,end=" ")
+    print(1,end="\n")
+        
